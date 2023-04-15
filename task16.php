f0d2e287561b2a0d9ec1f642171a2e41da780d10
Bismaka Aurent Faradyan <85272914+bfaradyan@users.noreply.github.com>
2023-04-15 16:01:19 +0700

    First commit

---
+++ task16.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+    <body>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-sm p-3 mb-5">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Bismaka Aurent Faradyan</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ms-auto">
+                        <a class="nav-link active" aria-current="page" href="#">Task 16</a>
+                        <a class="nav-link" href="#">Task 17</a>
+                        <a class="nav-link" href="#">Task 18</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <header class="container text-center">
+            <img src="/assets/profilepic.jpg" class="img-thumbnail rounded-circle" alt="Profile Picture" width='400' height='400'>
+            <h1>Bismaka Aurent Faradyan</h1>
+            <div class="list-group list-group-flush">
+                <a href="#" class="list-group-item list-group-item-action">Situbondo, March 16th 2002</a>
+                <a href="#" class="list-group-item list-group-item-action">Male</a>
+                <a href="#" class="list-group-item list-group-item-action">East Mastrip 105, Sumbersari, Kab. Jember, East Java</a>
+                <a href="#" class="list-group-item list-group-item-action">Music, Football, Movies, Books</a>
+            </div>
+            <h3 class="mt-4">Education</h3>
+            <ul class="list-group">
+                <li class="list-group-item">SMAN 1 Situbondo</li>
+                <li class="list-group-item">University of Jember</li>
+                <li class="list-group-item">Hacktiv8</li>
+            </ul>
+        </header>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    </body>
+</html>
